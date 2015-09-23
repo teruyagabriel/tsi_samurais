a9280cb6842fdfd3429396228294e3a07cf9a5b9
Hendy Almeida <hendy.almeida@hotmail.com>
2015-09-23 20:40:05 -0300

    iniciando tela de login

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>kanino - login</title>
+</head>
+<body>
+	<div class="content">
+		<div claa="logo">
+			<img src="logo.png">
+		</div>
+		<form class="login">
+			<input type="text" name="username"/>
+			<input type="password" name="password"/>
+			<button>Entrar</button>
+		</form>
+	</div>
+</body>
+</html>
+
+
