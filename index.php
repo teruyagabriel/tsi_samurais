2621fce9598ef32de5605699aef3d3590253026c
Hendy Almeida <hendy.almeida@hotmail.com>
2015-09-24 16:06:49 -0300

    deixa eu fazer um push aqui pra ver se funciona

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 <title>kanino - login</title>
 </head>
 <body> 
-	<div class="content">
-		<div claa="logo">
-			<img src="logo.png">
-		</div>
-		<form action="actions/login.php" class="form-login" id="form-login">
-			<input type="text" name="username"/>
-			<input type="password" name="password"/>
-			<button type="submit">Entrar</button>
-		</form>
-	</div>
-	 <!--teste (:-->
+<div class="content">
+    <div class="logo">
+        <img src="logo.png">
+    </div>
+    <form action="actions/login.php" class="form-login" id="form-login">
+        <input type="text" name="username"/>
+        <input type="password" name="password"/>
+        <button type="submit">Entrar</button>
+    </form>
+</div>
+ <!--teste (:-->
 </body>
 </html>
