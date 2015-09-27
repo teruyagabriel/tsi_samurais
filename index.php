49da4fb0a16d6a97ad6d1303ca338fb9c70f876e
teruyagabriel <teruyagabriel@yahoo.com.br>
2015-09-27 15:13:28 -0300

    Update index.php
    alterações para o css

--- index.php
+++ index.php
@@ -3,6 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Kanino - Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+    <div class="content">
+        <div class="logo">
+        </div>
+            <div id="acomodar">
+                <form action="actions/login.php" method="POST" class="form-login" id="form-login">
+                    <input type="text" name="username" id="nome" class="valor" placeholder="Nome">
+                    <input type="password" name="password" id="senha" class="valor" placeholder="Senha">
+                    <button type="submit" id="enviar">Entrar</button>
+                </form>
+            </div>
+        </div>
 </head>
 <body>
 <div class="content">
